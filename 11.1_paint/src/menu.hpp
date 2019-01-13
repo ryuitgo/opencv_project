f65940c8e1d4332d46aa2882bdd8e41ff34ff7f2
ryuitgo <ryuitgo@gmail.com>
2019-01-14 01:42:58 +0900

    [#1] 아이콘 이미지 수

--- 11.1_paint/src/menu.hpp
+++ 11.1_paint/src/menu.hpp
@@ -9,16 +9,21 @@ void place_icons(cv::Size size)
 
     int btn_rows = (int)cvCeil(icon_name.size() / 2.0);
 
-    for(int i = 0, k = 0; i < btn_rows; ++i) {
-        for(int j = 0; j < 2; ++j,++k) {
+    for(int i = 0, k = 0; i < btn_rows; i++) {
+        for(int j = 0; j < 2; j++,k++) {
+
             cv::Point pt(j * size.width, i * size.height);
+
             icons.push_back(cv::Rect(pt,size));
 
-            cv::Mat icon = cv::imread("../images/icon/" + icon_name[k] + ".png", 0);
+            cv::Mat icon = cv::Mat(70, 70, CV_8UC3, cv::Scalar(255,255,255));
+            cv::putText(icon, icon_name[k], cv::Point(10,10), 1, 1, cv::Scalar(200,55,55));
+
             if(icon.empty())
                 continue;
 
             resize(icon, icon, size);
+
             icon.copyTo(image(icons[k]));
         }
     }
