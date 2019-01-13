cf8b5721e678bafe8288a8fe7a5bc47dae6b5f55
ryuitgo <ryuitgo@gmail.com>
2019-01-13 22:51:29 +0900

    [#1] main function

--- 11.1_paint/src/main.cpp
+++ 11.1_paint/src/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "opencv2/opencv.hpp"
 
 using namespace std;
 
+std::vector<cv::Rect> icons;
+cv::Mat image;
+
+#include "menu.hpp"
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    image = cv::Mat(500, 800, CV_8UC3, cv::Scalar(255,255,255));
+    place_icons(cv::Size(60,60));
+
+//    cv::Rect last_icon = *(icons.end() -1);
+//    cv::Point start_pale = cv::Point(0, last_icon.br().y + 5);
+
+//    icons.push_back(cv::Rect(start_pale, cv::Size(100, 100)));
+//    icons.push_back(cv::Rect(start_pale + cv::Point(105,0), cv::Size(15,100)));
+
+//    cv::imshow("image", image);
+//    cv::waitKey(0);
     return 0;
 }
--- 11.1_paint/src/paint.pro
+++ 11.1_paint/src/paint.pro
@@ -6,7 +6,7 @@ CONFIG -= qt
 linux {
     message(==== linux ====)
     INCLUDEPATH += /home/ryuitgo/dev/3rd/opencv/opencv-4.0.0/include/opencv4
-    LIBS += -L/home/ryuitgo/dev/3rd/opencv/opencv-4.0.0/lib -lopencv_core -lopencv_imgproc -lopencv_imgcodecs
+    LIBS += -L/home/ryuitgo/dev/3rd/opencv/opencv-4.0.0/lib -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -lopencv_highgui
 }
 
 SOURCES += main.cpp
